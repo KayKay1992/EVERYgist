8238614e6ccb2419242e3534604b6344d17f42f4
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-11-10 04:35:27 -0800

    SignUp Component Implementation

--- frontend/gist_hub/src/components/Auth/SignUp.jsx
+++ frontend/gist_hub/src/components/Auth/SignUp.jsx
@@ -1,9 +1,125 @@
-import React from 'react'
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../utils/axioInstance";
+import { UserContext } from "../../context/userContext";
+import { API_PATHS } from "../../utils/apiPaths";
+import AUTH_IMG from "../../assets/auth-img-blog.jpg";
+import Input from "../Inputs/Input";
+import { validateEmail } from "../../utils/helper";
 
-const SignUp = () => {
+const SignUp = ({ setCurrentPage }) => {
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [adminAccessToken, setAdminAccessToken] = useState("");
+  const [error, setError] = useState(null);
+  const { updateUser, setOpenAuthForm } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  //handle SignUp Form submit
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    let profileImageUrl = "";
+    if (!fullName) {
+      setError("Please enter your full name");
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter your password");
+      return;
+    }
+
+    setError(""); //clear previous errors
+
+    //SignUp API call
+    try {
+      
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
+        setError(error.response.data.message);
+      } else {
+        setError("An error occurred during sign up. Please try again.");
+      }
+    }
+  };
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="flex items-center h-[520px]">
+      <div className="w-[90vw] md:w-[43vw] p-7 flex flex-col justify-center">
+        <h3 className="text-lg font-semibold text-black">
+          {" "}
+          Create an account{" "}
+        </h3>
+        <p className="text-xs text-slate-700 mt-0.5 mb-6">
+          Please fill in the details below to create an account.
+        </p>
 
-export default SignUp
+        <form className="" onSubmit={handleSignUp}>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-5 ">
+            <Input
+              value={fullName}
+              onChange={({ target }) => setFullName(target.value)}
+              label="Full Name"
+              type="text"
+              placeholder="Enter your full name"
+            />
+
+            <Input
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+              label="Email"
+              type="email"
+              placeholder="Enter your email"
+            />
+
+            <Input
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+              label="Password"
+              type="password"
+              placeholder="Enter your password"
+            />
+
+            <Input
+              value={adminAccessToken}
+              onChange={({ target }) => setAdminAccessToken(target.value)}
+              label="Admin Access Token (Only for admin)"
+              type="number"
+              placeholder="Enter admin access token"
+            />
+          </div>
+          {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+          <button type="submit" className="btn-primary mt-4">
+            Sign Up
+          </button>
+
+          <p className="text-[13px] text-slate-800 mt-3">
+            Already have an account?{" "}
+            <button
+              className="font-medium text-primary underline cursor-pointer"
+              onClick={() => setCurrentPage("login")}
+            >
+              Log In
+            </button>
+          </p>
+        </form>
+      </div>
+      <div className="hidden md:block ">
+        <img src={AUTH_IMG} alt="Login" className="h-[520px] w-[43vw]" />
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
