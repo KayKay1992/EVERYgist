092461f8060b6215e971eda697d6a959eb8080f4
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-11-10 02:11:13 -0800

    Role-Based Private Routes (Frontend)

--- frontend/gist_hub/src/routes/PrivateRoutes.jsx
+++ frontend/gist_hub/src/routes/PrivateRoutes.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { useContext } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { UserContext } from "../context/userContext";
 
-const PrivateRoutes = ({allowedRoles}) => {
-    //will implement role based access later
-  return <Outlet />
-}
+const PrivateRoutes = ({ allowedRoles }) => {
+  //Implement role based private routes
+  const { user, loading } = useContext(UserContext);
 
-export default PrivateRoutes
+  if (loading) {
+    return <div>Loading...</div>; //show loading indicator while checking auth status
+  }
+  if (!user) {
+    return <Navigate to="/" replace />;
+  }
+  if (!allowedRoles.includes(user.role)) {
+    return <Navigate to="/" replace />;
+  }
+  return <Outlet />;
+};
+
+export default PrivateRoutes;
