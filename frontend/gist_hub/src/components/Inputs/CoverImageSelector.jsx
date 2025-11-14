32582fd334273e1c27ff113207ea8df0e87640ba
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-11-14 01:09:45 -0800

    Blog post editor page with markdown editor

---
+++ frontend/gist_hub/src/components/Inputs/CoverImageSelector.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import {useRef, useState} from 'react';
+import { LuTrash, LuFileImage } from 'react-icons/lu';
+
+const CoverImageSelector = ({image, setImage, preview, setPreview}) => {
+    const inputRef = useRef(null);
+    const [previewUrl, setPreviewUrl] = useState(null);
+
+    const handleImageChange = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            setImage(file);
+            const preview = URL.createObjectURL(file);
+            if (setPreview) {
+                setPreview(preview);
+            }
+            setPreviewUrl(preview);
+        }
+    };
+
+    const handleRemoveImage = () => {
+        setImage(null);
+        setPreviewUrl(null);
+        if (setPreview) {
+            setPreview(null);
+        }
+    };
+
+    const onChooseFile = () => {
+        inputRef.current.click();
+    };
+  return <div className="mb-6">
+         <input
+            type="file"
+            accept="image/*"
+            ref={inputRef}
+            onChange={handleImageChange}
+           className='hidden'
+        />
+    {!image || !preview ? (
+        <div
+            className="w-full h-56 flex flex-col items-center justify-center gap-2 bg-gray-50/50 border border-dashed border-gray-300 relative rounded-md cursor-pointer hover:bg-sky-50 transition-all"
+            onClick={onChooseFile}
+        >
+            <div className="w-14 h-14 flex items-center  justify-center bg-sky-50 rounded-full">
+                <LuFileImage className="text-xl text-sky-600" />
+               
+            </div>
+             <p className="text-sm text-gray-700">Click to select cover image</p>
+        </div>
+    ) : (
+        <div className="relative w-full h-56 ">
+            <img
+                src={preview || previewUrl}
+                alt="Cover Preview"
+                className="w-full h-full object-cover rounded-md"
+            />
+            <button
+                className="absolute top-2 right-2 bg-red-500 text-whitetext-red-500 rounded-full p-2 border border-red-500 hover:border-red-500 transition-all shadow-md cursor-pointer"
+                onClick={handleRemoveImage}
+                type='button'> <LuTrash /></button>
+        </div>
+    )}
+</div>
+
+}
+
+export default CoverImageSelector
--- frontend/gist_hub/src/pages/Admin/BlogPostEditor.jsx
+++ frontend/gist_hub/src/pages/Admin/BlogPostEditor.jsx
@@ -1,9 +1,181 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+import MDEditor, { commands } from "@uiw/react-md-editor";
+import {
+  LuLoaderCircle,
+  LuSave,
+  LuSend,
+  LuSparkles,
+  LuTrash2,
+} from "react-icons/lu";
+import axiosInstance from "../../utils/axioInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import { useNavigate, useParams } from "react-router-dom";
+import CoverImageSelector from "../../components/Inputs/CoverImageSelector";
 
-const BlogPostEditor = () => {
+const BlogPostEditor = ({ isEdit }) => {
+  const navigate = useNavigate();
+  const { postSlug } = useParams();
+  const [postData, setPostData] = useState({
+    id: "",
+    title: "",
+    content: "",
+    coverImageUrl: "",
+    coverPreview: "",
+    tags: "",
+    isDraft: "",
+    generatedByAI: false,
+  });
+  const [postIdeas, setPostIdeas] = useState([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const [openBlogPostGenForm, setOpenBlogPostGenForm] = useState({
+    open: false,
+    data: null,
+  });
+  const [ideaLoading, setIdeaLoading] = useState(false);
+  const [openDeleteAlert, setOpenDeleteAlert] = useState(false);
+
+  const handleValueChange = (key, value) => {
+    setPostData((prevData) => ({
+      ...prevData,
+      [key]: value,
+    }));
+  };
+
+  //Generate blog post ideas using AI
+  const generatePostIdeas = async () => {};
+
+  //Handle blog post publish
+  const handlePublish = async (isDraft) => {};
+
+  //Get Post data by slug
+  const fetchPostDetailsBySlug = async () => {};
+
+  //Delete blog post
+  const deletePost = async () => {};
+
+  useEffect(() => {
+    if (isEdit) {
+      fetchPostDetailsBySlug();
+    } else {
+      generatePostIdeas();
+    }
+    return () => {};
+  }, [isEdit, postSlug]);
+
   return (
-    <div>BlogPostEditor</div>
-  )
-}
+    <DashboardLayout activeMenu="Blog Posts">
+      <div className="my-5">
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-12 my-4">
+          <div className="form-card p-6 col-span-12 md:col-span-8 lg:col-span-9">
+            <div className="flex items-center justify-between ">
+              <h2 className="text-base md:text-large font-medium">
+                {isEdit ? "Edit Post" : "Add New Post"}
+              </h2>
+              <div className="flex items-center gap-3">
+                {isEdit && (
+                  <button
+                    className="flex items-center gap-1.5 text-[13px] font-medium text-rose-500 bg-rose-50/60 rounded px-1.5 md:px-3 py-1 md:py-[3px] border border-rose-50 hover:border-rose-300 cursor-pointer hover:scale-[1.02] transition-all"
+                    disabled={loading}
+                    onClick={() => setOpenDeleteAlert(true)}
+                  >
+                    <LuTrash2 className="text-sm" />{" "}
+                    <span className="hidden md:block">Delete</span>
+                  </button>
+                )}
 
-export default BlogPostEditor
+                <button
+                  className="flex items-center gap-2.5 text-[13px] text-sky-500 font-medium bg-sky-50/60 rounded px-1.5 md:px-3 py-1 md:py-[3px] border border-sky-100 hover:border-sky-400 cursor-pointer hover:scale-105 transition-all"
+                  disabled={loading}
+                  onClick={() => handlePublish(true)}
+                >
+                  <LuSave className="text-sm" />{" "}
+                  <span className="hidden md:block">Save as Draft</span>
+                </button>
+
+                <button
+                  className="flex items-center gap-2.5 text-[13px] text-sky-600 font-medium bg-sky-50/60 rounded px-1.5 md:px-3 py-1 md:py-[3px] border border-sky-500 hover:bg-sky-50 cursor-pointer hover:scale-105 transition-all"
+                  disabled={loading}
+                  onClick={() => handlePublish(false)}
+                >
+                  {loading ? (
+                    <LuLoaderCircle className="ansimate-spin text-[15px]" />
+                  ) : (
+                    <LuSend className="text-sm" />
+                  )}
+                  <span className="">
+                    {isEdit ? "Update & Publish" : "Publish Post"}
+                  </span>
+                </button>
+              </div>
+            </div>
+            {error && <p className="text-xs text-red-600 pb-2.5">{error}</p>}
+
+            <div className="mt-4">
+              <label className="text-xs font-medium text-slate-600">
+                Post Title
+              </label>
+              <input
+                type="text"
+                value={postData.title}
+                onChange={({ target }) =>
+                  handleValueChange("title", target.value)
+                }
+                className="form-input"
+                placeholder="Enter your blog post title"
+              />
+            </div>
+
+            <div className="mt-4">
+              <CoverImageSelector
+                image={postData.coverImageUrl}
+                setImage={(value) => handleValueChange("coverImageUrl", value)}
+                preview={postData.coverPreview}
+                setPreview={(value) => handleValueChange("coverPreview", value)}
+              />
+            </div>
+
+            <div className="mt-3">
+              <label className="text-xs font-medium text-slate-600">Content</label>
+
+              <div data-color-mode="light" className="mt-3">
+                <MDEditor
+                  value={postData.content}
+                  onChange={(data) => handleValueChange("content", data)}
+                  commands={[
+                    commands.bold,
+                    commands.italic,
+                    commands.strikethrough,
+                    commands.hr,
+                    commands.quote,
+                    commands.divider,
+                    commands.link,
+                    commands.code,
+                    commands.image,
+                    commands.unorderedListCommand,
+                    commands.orderedListCommand,
+                    commands.checkedListCommand,
+                    commands.heading,
+                    commands.heading1,
+                    commands.heading2,
+                    commands.heading3,
+                    commands.heading4,
+                    commands.heading5,
+                    commands.heading6,
+                    
+                  ]}
+                  hideMenuBar={true}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </DashboardLayout>
+  );
+};
+
+export default BlogPostEditor;
